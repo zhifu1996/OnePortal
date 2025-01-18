fea80fca5c5ef4d9b44572480abb7f2fa6dfed93
EFLKumo <lapped-spent-blimp@duck.com>
2025-01-18 17:53:22 +0800

    refactor: shorten the name fix(player): use dynamic import to prevent local running problems chore: add wrangler.toml to .gitignore chore: update dependencies

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -1,10 +1,10 @@
 import { KVNamespace } from '@cloudflare/workers-types'
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
+  const { ODL_KV } = process.env as unknown as { ODL_KV: KVNamespace }
 
-  const accessToken = await ONEDRIVE_CF_INDEX_KV.get('access_token')
-  const refreshToken = await ONEDRIVE_CF_INDEX_KV.get('refresh_token')
+  const accessToken = await ODL_KV.get('access_token')
+  const refreshToken = await ODL_KV.get('refresh_token')
 
   return {
     accessToken,
@@ -21,8 +21,8 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
-  const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
+  const { ODL_KV } = process.env as unknown as { ODL_KV: KVNamespace }
 
-  await ONEDRIVE_CF_INDEX_KV.put('access_token', accessToken, { expirationTtl: accessTokenExpiry })
-  await ONEDRIVE_CF_INDEX_KV.put('refresh_token', refreshToken)
+  await ODL_KV.put('access_token', accessToken, { expirationTtl: accessTokenExpiry })
+  await ODL_KV.put('refresh_token', refreshToken)
 }

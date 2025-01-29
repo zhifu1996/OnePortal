18815d5742bce3c847c91b46904a3efa4d4e2a3e
EFLKumo <lapped-spent-blimp@duck.com>
2025-01-29 09:33:52 +0800

    fix: missing import

--- src/components/previews/Containers.tsx
+++ src/components/previews/Containers.tsx
@@ -1,3 +1,5 @@
+import { JSX } from 'react'
+
 export function PreviewContainer({ children }): JSX.Element {
   return <div className="rounded bg-white p-3 shadow-sm dark:bg-gray-900 dark:text-white">{children}</div>
 }
